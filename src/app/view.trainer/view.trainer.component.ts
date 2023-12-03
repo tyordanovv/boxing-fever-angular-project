1196b429cb9b93c1a3955e1dd71e01726e478084
Omar Oraby <52020494@edu.campus02.at>
2023-12-03 15:39:46 +0100

    view trainers

--- src/app/view.trainer/view.trainer.component.ts
+++ src/app/view.trainer/view.trainer.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, inject } from '@angular/core';
 import { TrainerServiceService } from '../_services/trainer.service.service';
 import { MatDialog,  } from '@angular/material/dialog';
 import { ConfirmationDialogComponent } from 'src/app/util/confirmation-dialog/confirmation-dialog.component';
+import { TrainerModel } from '../model/trainer.model';
 
 @Component({
   selector: 'app-view.trainer',
@@ -9,45 +10,26 @@ import { ConfirmationDialogComponent } from 'src/app/util/confirmation-dialog/co
   styleUrls: ['./view.trainer.component.css']
 })
 export class ViewTrainerComponent {
-  trainer: any;
-  private dialog = inject(MatDialog);
+  trainers: TrainerModel[] = [];
   errorMessage = '';
 
-  constructor( 
-    private trainerService: TrainerServiceService,
-  ) {}
+  constructor(private trainerService: TrainerServiceService) { }
 
-  getAllTrainerInfo(): void {
+  ngOnInit(): void {
+    this.fetchTrainers();
+  }
+
+  private fetchTrainers(): void {
     this.trainerService.getTrainers().subscribe({
       next: data => {
-        this.trainer = data
+        this.trainers = data;
       },
       error: err => {
         this.errorMessage = err.error.message;
       }
-    }
-
-    );
-  }
-
-  confirmDelete(): void {
-    const message = 'Are you sure you want to delete the trainer?';
-    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      data: message 
-    })
-
-    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
-      if (confirmed) {
-        const trainerId = this.trainer.id;
-
-        this.trainerService.deleteTrainer(trainerId).subscribe({
-          error: err => {
-            this.errorMessage = err.error.message;
-          }
-        });
-      }
     });
   }
+
 
   reloadPage(): void {
     window.location.reload();
