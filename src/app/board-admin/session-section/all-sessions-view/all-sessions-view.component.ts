13f95531099d2bef867a90ae8aa6136f0e71d475
ReneHofer <s51165@edu.campus02.at>
2023-12-03 15:41:13 +0100

    Delete and Subscribe Sessions

--- src/app/board-admin/session-section/all-sessions-view/all-sessions-view.component.ts
+++ src/app/board-admin/session-section/all-sessions-view/all-sessions-view.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import { sessionModel} from "../../../model/session.model";
 import { SessionService} from "../../../_services/session-service";
+import {StorageService} from "../../../_services/storage.service";
 
 @Component({
   selector: 'app-all-sessions-view',
@@ -14,7 +15,9 @@ export class AllSessionsViewComponent implements OnInit {
   @Input() sessions!: sessionModel[];
   @Output() signUpClicked = new EventEmitter<void>();
 
-  constructor( private sessionService: SessionService) {
+  successMessages: { [classId: number]: string } = {};
+
+  constructor( private sessionService: SessionService, private storageService: StorageService) {
   }
 
   ngOnInit(): void {
@@ -33,4 +36,33 @@ export class AllSessionsViewComponent implements OnInit {
     );
 
   }
+  subscribeToClass(sessionId: number, userID: number ): void {
+    this.sessionService.subscribeToClass(sessionId, userID).subscribe(
+      () => {
+        const sessionToUpdate = this.sessions.find((session) => session.id === sessionId);
+        if (sessionToUpdate){
+          sessionToUpdate.capacity -= 1;
+        }
+        this.successMessages[sessionId] = 'Subscribed to class successfully.'
+        console.log('Subscribed to class successfully.');
+
+      },
+      (error) => {
+        this.successMessages[sessionId] = 'Could not subscribe to class.'
+        console.error('Error subscribing to class:', error);
+      }
+    );
+  }
+
+  getSuccessMessage(classId: number): string {
+    return this.successMessages[classId] || '';
+  }
+  getUserID(): number {
+    if (this.storageService.isLoggedIn()) {
+      return this.storageService.getUser().user.id;
+  } else {
+      return -1;
+    }
+}
+
 }
